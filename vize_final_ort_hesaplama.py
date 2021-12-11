36b20d7bbdb507200b0c6ea50bbc3e147b02d2c7
Berkant Beşmart <berkant.5m@hotmail.com>
2021-12-11 12:07:05 +0300

    Add files via upload

---
+++ vize_final_ort_hesaplama.py
@@ -0,0 +1,4 @@
+vize = input('Vize Notunuzu Giriniz : ')
+final = input('Final Notunuzu Giriniz : ')
+ortalama = (float(vize) * 0.4) + (float(final) * 0.6)
+print("Ortalamanız :{0} ".format(ortalama))
